3cd7de07f27713e60eab1f5cdca02b3b0f0304bb
eon <bernardomp.dev@gmail.com>
2023-08-28 15:45:46 -0300

    Feat: Application.

--- src/main/java/com/betrybe/bankaccount/Application.java
+++ src/main/java/com/betrybe/bankaccount/Application.java
@@ -1,15 +1,28 @@
 package com.betrybe.bankaccount;
 
+import java.util.Scanner;
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * A class.
  */
 public class Application {
 
-  /**
-   * A simple get method.
-   */
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
 
+    System.out.println("Informe o número da conta:");
+    String input = scanner.nextLine();
+
+    if (!StringUtils.isNumeric(input)) {
+      System.out.println("Número da conta inválido!");
+      return;
+    }
+
+    AccountNumberFormatter formatter = new AccountNumberFormatter();
+    String formattedAccount = formatter.formatAccountNumber(Integer.parseInt(input));
+
+    System.out.println("Número da conta: " + formattedAccount);
   }
-
 }
+
