77376fd9744da712ead41a6513c2259def1e12c2
eon <bernardomp.dev@gmail.com>
2023-08-28 15:47:18 -0300

    Fix: CheckStyle.

--- src/main/java/com/betrybe/bankaccount/Application.java
+++ src/main/java/com/betrybe/bankaccount/Application.java
@@ -8,6 +8,9 @@ import org.apache.commons.lang3.StringUtils;
  */
 public class Application {
 
+  /**
+   * Main.
+   */
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
